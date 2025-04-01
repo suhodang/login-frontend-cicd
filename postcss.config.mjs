14d44d7782f8f843f2b6c372a734948896e13138
태현 <78199616+NekoNyangYee@users.noreply.github.com>
2025-04-02 01:51:31 +0900

    fix: main.yml 수정 13

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,6 @@
-// postcss.config.mjs
-import tailwindcss from "tailwindcss";
+import tailwindcss from "@tailwindcss/postcss";
 import autoprefixer from "autoprefixer";
 
-/** @type {import('postcss-load-config').Config} */
 export default {
   plugins: [tailwindcss(), autoprefixer()],
 };
